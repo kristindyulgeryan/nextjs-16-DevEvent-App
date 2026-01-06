cc941d42768cfee462b01aca497f52cafe810572
Kristin <kristindyulgeryan@abv.bg>
2026-01-06 13:46:36 +0200

    Add EventCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/Explorebtn";
+
+const events = [
+  {
+    image: "/images/event1.png",
+    title: "Event 1",
+    slug: "event-1",
+    location: "location-1",
+    date: "Date-1",
+    time: "Time-1",
+  },
+];
+
 const Page = () => {
   return (
     <section>
@@ -7,6 +21,17 @@ const Page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in One Place
       </p>
+      <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
